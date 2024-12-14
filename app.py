45f9c01a58ca86706dc9c8f4432a02e7942d044f
Wilson Filho <will.flho@gmail.com>
2024-12-14 10:22:57 -0300

    Add sleep disorder chart

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.markdown ("# Saúde do Sono e Estilo de Vida")
 
 st.markdown('## Dados')
 data = pd.read_csv('dataset.csv')
-st.write(data)
+
+st.header("Correlação: Distúrbios do Sono, Duração do Sono e Ocupação")
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='Occupation', y='SleepDuration', hue='SleepDisorder', data=data)
+plt.title("Duração do Sono por Ocupação e Presença de Distúrbios do Sono")
+plt.xlabel("Ocupação")
+plt.ylabel("Duração do Sono (horas)")
+plt.xticks(rotation=45)
+st.pyplot(plt)
 
 st.header("Atividade Física vs. Duração do Sono")
 plt.figure(figsize=(10, 5))
@@ -16,10 +25,12 @@ plt.xlabel("Nível de Atividade Física")
 plt.ylabel("Horas de Sono")
 st.pyplot(plt)
 
-st.header("Distribuição das Horas de Sono")
-plt.figure(figsize=(10, 5))
-plt.hist(data['SleepDuration'], bins=20, alpha=0.7, edgecolor='black')
-plt.title("Distribuição de Duração do Sono")
-plt.xlabel("Horas de Sono")
-plt.ylabel("Frequência")
-st.pyplot(plt)
+st.write(data)
+
+# st.header("Distribuição das Horas de Sono")
+# plt.figure(figsize=(10, 5))
+# plt.hist(data['SleepDuration'], bins=20, alpha=0.7, edgecolor='black')
+# plt.title("Distribuição de Duração do Sono")
+# plt.xlabel("Horas de Sono")
+# plt.ylabel("Frequência")
+# st.pyplot(plt)
